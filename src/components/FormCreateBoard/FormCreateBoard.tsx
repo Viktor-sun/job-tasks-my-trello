4eee8c270856905a77b733c56a6e069405deab85
Viktor-sun <bobick.zdoh@gmail.com>
2021-12-30 12:10:21 +0200

    feat: add formik & yup

--- src/components/FormCreateBoard/FormCreateBoard.tsx
+++ src/components/FormCreateBoard/FormCreateBoard.tsx
@@ -1,27 +1,45 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import routes from "../../routes";
 import { boardActions } from "../../redux/actions";
 
 const FormCreateBoard = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [inputValue, setInputValue] = useState("");
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setInputValue(e.currentTarget.value);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(boardActions.changeTitle.Request(inputValue));
-    navigate(routes.board);
-  };
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: Yup.object({
+      title: Yup.string()
+        .trim("need trim")
+        .strict()
+        .required("👋Board title is required")
+        .min(2, "needs to be at least two characters"),
+    }),
+    onSubmit: (values) => {
+      dispatch(boardActions.changeTitle.Request(values.title));
+      navigate(routes.board);
+      formik.resetForm();
+    },
+  });
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <label>
         Board title
-        <input type="text" value={inputValue} onChange={handleChange} />
+        <input
+          type="text"
+          name="title"
+          onChange={formik.handleChange}
+          value={formik.values.title}
+        />
+        {formik.errors.title && formik.touched.title ? (
+          <div style={{ color: "red" }}>{formik.errors.title}</div>
+        ) : null}
       </label>
       <button type="submit">submit</button>
     </form>
