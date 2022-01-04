d23e9ec064d7f1b12af2b5d598d5dd637902b61b
Viktor-sun <bobick.zdoh@gmail.com>
2022-01-04 11:37:44 +0200

    add modern-normalize

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import "./index.css";
+import "modern-normalize/modern-normalize.css";
 import App from "./App";
 
 ReactDOM.render(
