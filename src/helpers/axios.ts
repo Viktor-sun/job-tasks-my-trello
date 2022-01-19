28914a53f87d0c2cad32d336fb00b06d6d13e31d
Viktor-sun <bobick.zdoh@gmail.com>
2022-01-19 10:39:36 +0200

    add types

--- src/helpers/axios.ts
+++ src/helpers/axios.ts
@@ -1,19 +1,24 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig, AxiosInstance, AxiosResponse } from "axios";
 
 const API_URL = "http://localhost:8050/api";
 
-const customApi = axios.create({
+const customApi: AxiosInstance = axios.create({
   withCredentials: true,
   baseURL: API_URL,
 });
 
-customApi.interceptors.request.use((config) => {
+customApi.interceptors.request.use((config: AxiosRequestConfig) => {
+  if (!config?.headers) {
+    throw new Error(
+      `Expected 'config' and 'config.headers' not to be undefined`
+    );
+  }
   config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   return config;
 });
 
 customApi.interceptors.response.use(
-  (config) => config,
+  (config: AxiosResponse) => config,
   async (error) => {
     const originalRequest = error.config;
 
@@ -31,7 +36,7 @@ customApi.interceptors.response.use(
         return customApi.request(originalRequest);
       } catch (e) {
         console.log(e);
-        console.log("not auth");
+        console.log("unauthorized");
       }
     }
 
